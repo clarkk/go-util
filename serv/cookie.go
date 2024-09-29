d056096658a8156c76f526f75627711c30e9aee0
clarkk <clark@electrobeat.dk>
2024-09-29 22:27:48 +0000

    added serv.Set_cookie_script()

--- serv/cookie.go
+++ serv/cookie.go
@@ -15,15 +15,28 @@ func Set_cookie(w http.ResponseWriter, name string, value string, max_age int){
 	})
 }
 
+//	Set cookie on client without HttpOnly
+func Set_cookie_script(w http.ResponseWriter, name string, value string, max_age int){
+	http.SetCookie(w, &http.Cookie{
+		Name:		name,
+		Value:		value,
+		MaxAge:		max_age,
+		Path:		"/",
+		SameSite:	http.SameSiteLaxMode,
+		Secure:		true,
+		HttpOnly:	false,
+	})
+}
+
 //	Delete cookie on client
 func Delete_cookie(w http.ResponseWriter, name string){
 	http.SetCookie(w, &http.Cookie{
 		Name:		name,
 		Value:		"",
 		MaxAge:		-1,
-		Path:		"/",
-		SameSite:	http.SameSiteLaxMode,
-		Secure:		true,
-		HttpOnly:	true,
+		//Path:		"/",
+		//SameSite:	http.SameSiteLaxMode,
+		//Secure:		true,
+		//HttpOnly:	true,
 	})
 }
