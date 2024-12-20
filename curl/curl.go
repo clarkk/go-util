27583fc250c4ff77737f8b44f473149f3ef9a432
clarkk <clark@electrobeat.dk>
2024-12-20 12:05:58 +0000

    update curl

--- curl/curl.go
+++ curl/curl.go
@@ -7,22 +7,22 @@ import (
 	"github.com/go-json-experiment/json"
 )
 
-func Curl_JSON(url string, input any, timeout int) error {
+func Curl_JSON(url string, input any, timeout int) (int, error){
 	client := http.Client{
 		Timeout: time.Duration(timeout) * time.Second,
 	}
 	r, err := client.Get(url)
 	if err != nil {
-		return err
+		return 0, err
 	}
 	defer r.Body.Close()
 	
 	b, err := io.ReadAll(r.Body)
 	if err != nil {
-		return err
+		return r.StatusCode, err
 	}
 	if err := json.Unmarshal(b, input); err != nil {
-		return err
+		return r.StatusCode, err
 	}
-	return nil
+	return r.StatusCode, nil
 }
