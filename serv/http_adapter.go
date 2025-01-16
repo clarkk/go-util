c8ffa315fe4eed117db7dddb12df55ffd427c7b0
clarkk <clark@electrobeat.dk>
2025-01-16 20:57:47 +0000

    update

--- serv/http_adapter.go
+++ serv/http_adapter.go
@@ -2,7 +2,7 @@ package serv
 
 import "net/http"
 
-type Adapter func(h http.Handler) http.Handler
+type Adapter func(http.Handler) http.Handler
 
 func Adapt(h http.HandlerFunc, wrappers... Adapter) http.HandlerFunc {
 	for _, wrapper := range wrappers {
