2c8291efb70906926a86890d802357e9d7106097
clarkk <clark@electrobeat.dk>
2025-03-20 12:32:06 +0000

    hash

--- hash/hash.go
+++ hash/hash.go
@@ -1,15 +1,9 @@
 package hash
 
 import (
-	"crypto/sha1"
 	"crypto/sha256"
 	"encoding/hex"
 )
-
-func SHA1_hex(b []byte) string {
-	sum := sha1.Sum(b)
-	return hex.EncodeToString(sum[:])
-}
 
 func SHA256_hex(b []byte) string {
 	sum := sha256.Sum256(b)
