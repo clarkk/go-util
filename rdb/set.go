d069c8e31e55d6d1e0fca01ab6446b36b341b1fe
clarkk <clark@electrobeat.dk>
2025-07-12 15:03:04 +0000

    update

--- rdb/set.go
+++ rdb/set.go
@@ -3,12 +3,11 @@ package rdb
 import (
 	"time"
 	"context"
-	//"github.com/redis/go-redis/v9"
 )
 
 //	Store members in set
 func SAdd(ctx context.Context, key string, values []any, expires int) error {
-	if err := client.Sadd(ctx, key, values...).Err(); err != nil {
+	if err := client.SAdd(ctx, key, values...).Err(); err != nil {
 		return err
 	}
 	client.Expire(ctx, key, time.Duration(expires) * time.Second)
