ad4dd84a84c4e75d640837b6f5aa95dba6d180c5
clarkk <clark@electrobeat.dk>
2025-08-31 09:03:32 +0000

    serv adapter

--- serv/http_adapter.go
+++ serv/http_adapter.go
@@ -2,11 +2,11 @@ package serv
 
 import "net/http"
 
-type Adapter func(http.Handler) http.Handler
+type Adapter func(http.HandlerFunc) http.HandlerFunc
 
 func Adapt(h http.HandlerFunc, wrappers... Adapter) http.HandlerFunc {
 	for _, wrapper := range wrappers {
-		h = wrapper(h).ServeHTTP
+		h = wrapper(h)
 	}
 	return h
 }
