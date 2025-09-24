0c568f2fc1803728f5f1bf670c61ff002f99f25a
clarkk <clark@electrobeat.dk>
2025-09-24 08:38:18 +0000

    lang

--- lang/printer.go
+++ lang/printer.go
@@ -8,6 +8,6 @@ type Printer struct {
 	*message.Printer
 }
 
-func (p *Printer) Number(){
-	
+func (p *Printer) Number_int64(i, factor int64) string {
+	return p.Sprintf("%.2f", float64(i / factor))
 }
