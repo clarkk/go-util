48a48cf6bf4c54a08284c173bd864a9fefd5aff2
clarkk <clark@electrobeat.dk>
2025-10-08 07:15:41 +0000

    added hmac sha256

--- hash/hash.go
+++ hash/hash.go
@@ -1,6 +1,7 @@
 package hash
 
 import (
+	"crypto/hmac"
 	"crypto/sha256"
 	"encoding/hex"
 )
@@ -8,4 +9,10 @@ import (
 func SHA256_hex(b []byte) string {
 	sum := sha256.Sum256(b)
 	return hex.EncodeToString(sum[:])
+}
+
+func HMAC_SHA256_hex(key, msg []byte) string {
+	mac := hmac.New(sha256.New, key)
+	mac.Write(msg)
+	return hex.EncodeToString(mac.Sum(nil))
 }
