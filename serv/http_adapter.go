ef1c828aa6eb9ab7fcda839099dd30b3886e4906
clarkk <clark@electrobeat.dk>
2026-01-04 02:37:14 +0000

    udpate

--- serv/http_adapter.go
+++ serv/http_adapter.go
@@ -5,8 +5,8 @@ import "net/http"
 type Adapter func(http.HandlerFunc) http.HandlerFunc
 
 func Adapt(h http.HandlerFunc, wrappers... Adapter) http.HandlerFunc {
-	for i := len(wrappers) - 1; i >= 0; i-- {
-		h = wrappers[i](h)
+	for _, wrapper := range wrappers {
+		h = wrapper(h)
 	}
 	return h
 }
