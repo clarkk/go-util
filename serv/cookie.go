2719bd44c7975f2d151c5026c2eaba9b36f28df5
clarkk <clark@electrobeat.dk>
2026-01-04 21:17:42 +0000

    improved serv cookie

--- serv/cookie.go
+++ serv/cookie.go
@@ -4,51 +4,37 @@ import "net/http"
 
 //	Set cookie on client
 func Set_cookie(w http.ResponseWriter, name, value string, max_age int){
-	http.SetCookie(w, &http.Cookie{
-		Name:			name,
-		Value:			value,
-		Path:			"/",
-		MaxAge:			max_age,
-		SameSite:		http.SameSiteLaxMode,	// http.SameSiteStrictMode
-		Secure:			true,
-		HttpOnly:		true,
-	})
+	set_cookie(w, name, value, max_age, true)
 }
 
 //	Set session cookie on client
 func Set_cookie_session(w http.ResponseWriter, name, value string){
-	http.SetCookie(w, &http.Cookie{
-		Name:			name,
-		Value:			value,
-		Path:			"/",
-		MaxAge:			0,
-		SameSite:		http.SameSiteLaxMode,	// http.SameSiteStrictMode
-		Secure:			true,
-		HttpOnly:		true,
-	})
+	set_cookie(w, name, value, 0, true)
 }
 
 //	Set cookie on client without HttpOnly for javascript access
 func Set_cookie_script(w http.ResponseWriter, name, value string, max_age int){
-	http.SetCookie(w, &http.Cookie{
-		Name:			name,
-		Value:			value,
-		Path:			"/",
-		MaxAge:			max_age,
-		SameSite:		http.SameSiteLaxMode,	// http.SameSiteStrictMode
-		Secure:			true,
-		HttpOnly:		false,
-	})
+	set_cookie(w, name, value, max_age, false)
 }
 
 //	Delete cookie on client
 func Delete_cookie(w http.ResponseWriter, name string){
+	set_cookie(w, name, "", -1, true)
+}
+
+//	Delete cookie on client
+func Delete_cookie_script(w http.ResponseWriter, name string){
+	set_cookie(w, name, "", -1, false)
+}
+
+func set_cookie(w http.ResponseWriter, name, value string, max_age int, http_only bool) {
 	http.SetCookie(w, &http.Cookie{
-		Name:			name,
-		Value:			"",
-		Path:			"/",
-		MaxAge:			-1,
-		SameSite:		http.SameSiteLaxMode,	// http.SameSiteStrictMode
-		Secure:			true,
+		Name:     name,
+		Value:    value,
+		Path:     "/",
+		MaxAge:   max_age,
+		Secure:   true,
+		HttpOnly: http_only,
+		SameSite: http.SameSiteLaxMode,
 	})
 }
