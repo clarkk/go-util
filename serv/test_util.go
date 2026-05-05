1009af4057b39662976f4d1552e9eacbb0d889c3
clarkk <clark@electrobeat.dk>
2026-05-05 17:16:32 +0000

    update

--- serv/test_util.go
+++ serv/test_util.go
@@ -1,5 +1,10 @@
 package serv
 
+import (
+	"context"
+	"net/http"
+)
+
 func Test_set_slugs(r *http.Request, slugs ...string) *http.Request {
 	ctx := context.WithValue(r.Context(), ctx_slug, slugs)
 	return r.WithContext(ctx)
